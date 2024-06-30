f42deb942fb4076d111bdaf9f5f6be8fd29e61e6
lateppetal <abdulpetal03@gmail.com>
2024-06-30 16:49:29 +0700

    update

--- js/script.js
+++ js/script.js
@@ -14,3 +14,34 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Calculator
+const nishab = 6859394;
+
+function zakatprofesi() {
+  const pendapatan =
+    parseFloat(document.getElementById("pendapatan").value) || 0;
+  const pendapatanlain =
+    parseFloat(document.getElementById("pendapatanlain").value) || 0;
+  const hutang = parseFloat(document.getElementById("hutang").value) || 0;
+
+  const totalharta = pendapatan + pendapatanlain - hutang;
+  document.getElementById("totalharta").value = totalharta;
+
+  nilaizakat(totalharta);
+}
+
+function nilaizakat(totalharta) {
+  let nilaizakat = 0;
+  if (totalharta > nishab) {
+    nilaizakat = totalharta * 0.025;
+  }
+
+  document.getElementById("nilaizakat").value = nilaizakat;
+}
+
+document.getElementById("pendapatan").addEventListener("input", zakatprofesi);
+document
+  .getElementById("pendapatanlain")
+  .addEventListener("input", zakatprofesi);
+document.getElementById("hutang").addEventListener("input", zakatprofesi);
